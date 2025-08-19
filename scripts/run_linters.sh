d854ba5346efbcdeab234676aa06b559016c2f29
FoxOfFire <54199935+FoxOfFire@users.noreply.github.com>
2025-08-19 16:32:11 +0200

    added pyright as new linter

--- scripts/run_linters.sh
+++ scripts/run_linters.sh
@@ -14,6 +14,11 @@ if ! pyenv exec flake8 . ; then
         SCRIPT_RET=1
 fi
 
+echo "Running: pyright ."
+if ! pyenv exec pyright . ; then
+	SCRIPT_RET=1
+fi
+
 echo "Running: mypy ."
 if ! pyenv exec mypy . ; then
         SCRIPT_RET=1
