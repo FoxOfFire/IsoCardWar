2bc4aeeac3f73c5baca936f5f5b18e64001fc62e
Balázs Vince <54199935+FoxOfFire@users.noreply.github.com>
2025-11-27 09:52:29 +0100

    ??

--- scripts/init_env.sh
+++ scripts/init_env.sh
@@ -5,13 +5,13 @@ pyenv local 3.12.9
 echo "Version set to: $(pyenv exec python --version)"
 
 echo "Creating venv"
-pyenv exec python -m venv .venv
+pyenv exec python -m venv ./.venv
 
 echo "Activating"
-source .venv/bin/activate
+source ./.venv/bin/activate
 
 echo "Installing requirements:"
 pyenv exec pip install --upgrade pip
-pyenv exec pip install -r requirements.txt
+pyenv exec pip install -r ./requirements.txt
 
 exit 0
