02c8220cce4f3a9a46294aefc21d3bd9a335ee12
FoxOfFire <54199935+FoxOfFire@users.noreply.github.com>
2026-02-17 14:48:38 +0100

    toggle-none

--- core/spawners/spawner_actions.py
+++ core/spawners/spawner_actions.py
@@ -7,7 +7,7 @@ from layer2 import UIElementComponent
 def get_toggle_menu_visibility(
     menus: List[UIElementComponent], menu_num: int
 ) -> Action:
-    def toggle_menu_vis(_: ActionArgs) -> None:
+    def toggle_menu_vis(_: ActionArgs = None) -> None:
         assert menu_num >= 0 and menu_num < len(menus)
         menus[menu_num].is_visible = not menus[menu_num].is_visible
 
